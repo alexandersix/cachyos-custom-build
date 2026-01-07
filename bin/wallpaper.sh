c5e15274056d2ac5eca558481403d8d601187e6e
Alexander Six <alexanderhsix@gmail.com>
2026-01-07 17:02:58 -0500

    fix(bin): update wallpaper script to apply to all daemons

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-swww img -t random --transition-fps 60 $HOME/wallpapers/everforest/polyscape_2.png
+swww img -t -a random --transition-fps 60 $HOME/wallpapers/everforest/polyscape_2.png
